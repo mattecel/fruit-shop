881c9704c6f36f9705eed0f85ad61374aa10b384
Matthew Ecelbarger <matt.ecel123@gmail.com>
2022-08-16 12:44:20 -0400

    initial commit

---
+++ src/test/java/com/amex/fruitshop/FruitShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amex.fruitshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
